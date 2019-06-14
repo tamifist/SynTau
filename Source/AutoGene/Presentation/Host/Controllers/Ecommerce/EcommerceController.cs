7aa9919fbd2d49c6897b279fa91fd49720c43f05
Vlad Omelyanchuk <v.omelyanchuk@outlook.com>
2019-06-14 15:08:57 +0300

    sb-admin-2 ecommerce layout

---
+++ Source/AutoGene/Presentation/Host/Controllers/Ecommerce/EcommerceController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Business.Contracts.Services.GeneEditor;
+using Business.Contracts.ViewModels.GeneEditor;
+using Presentation.Common.Controllers;
+using Presentation.Common.Security;
+
+namespace AutoGene.Presentation.Host.Controllers.CycleEditor
+{
+    //[AuthorizeUser(UserRoles = "Guest,Admin")]
+    public class EcommerceController : BaseController
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
