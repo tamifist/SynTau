53a7489fb12f6c53e03464411ec71a06d4d9e470
Vlad Omelyanchuk <v.omelyanchuk@outlook.com>
2019-06-18 21:59:05 +0300

    Order Genes form

---
+++ Source/AutoGene/Presentation/Host/Controllers/Ecommerce/OrderGenesController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using Presentation.Common.Controllers;
+
+namespace AutoGene.Presentation.Host.Controllers.Ecommerce
+{
+    //[AuthorizeUser(UserRoles = "Guest,Admin")]
+    public class OrderGenesController : BaseController
+    {
+        [HttpPost]
+        public ActionResult Index(string genesProjectName)
+        {
+            return View();
+        }
+    }
+}
